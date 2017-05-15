31f4d56bf4e7bfe4810a01ee18062fa8f3f976af
fly <pingxxhxj@sina.cn>
2017-05-15 20:39:54 +0800

    set project status

---
+++ src/components/promote/promote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="promote">
+    <div class="page-message">
+      <h1>page message</h1>
+      <p class="message-item"></p>
+      <p class="message-item"></p>
+      <p class="message-item"></p>
+      <p class="message-item"></p>
+    </div>
+  </div>
+</template>
+
+<script type="ecmascript-6">
+  export default {
+
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
